788cd9bd0bef06ca5bf1089305cf70fe7b6e0e8d
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2024-03-15 23:39:02 -0500

    cambios

--- SistemaVenta.API/Program.cs
+++ SistemaVenta.API/Program.cs
@@ -28,9 +28,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("NuevaPolitica");
 
